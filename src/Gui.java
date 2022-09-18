e6083667abdc12eadf38248aae43cd1583f0678b
symone03 <112593865+symone03@users.noreply.github.com>
2022-09-18 19:40:44 -0400

    menu bar update

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,4 @@
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -31,25 +31,37 @@ public class Gui extends JFrame implements ActionListener {
     public Gui() {
         super("UMGC Western Blot Editor");
 
-        // Create Graphical Interface
+        // Create Graphical Interface (GUI)
+       //resize button
         buttonResize = new JButton("Resize");
         buttonResize.addActionListener(this);
+        
+      //EdgeDetector button
         buttonEdgeDetector = new JButton("EdgeDetector");
         buttonEdgeDetector.addActionListener(this);
+        
+      //Invert button
         buttonInvert = new JButton("Invert");
         buttonInvert.addActionListener(this);
+        
+       //Bright/Contrast button
         buttonBrightnessContrast = new JButton("Bright/Contrast");
         buttonInvert.addActionListener(this);
+        
+      //Reset button
         buttonReset = new JButton("Reset");
         buttonReset.addActionListener(this);
-
+        
         panelBottom = new JPanel();
+        
+        //Might not need panel buttons on bottom if we allow user to choose from menu bar 
+        
         panelBottom.add(buttonResize);
         panelBottom.add(buttonEdgeDetector);
         panelBottom.add(buttonInvert);
         panelBottom.add(buttonBrightnessContrast);
         panelBottom.add(buttonReset);
-
+        
         // ImagePanel
 
         Container l_c = getContentPane();
@@ -79,28 +91,62 @@ public class Gui extends JFrame implements ActionListener {
         // create a menu
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
-        JMenu tools = new JMenu("Tools");
+        JMenu tools = new JMenu("Image");
         // create menuitems
-        JMenuItem file1 = new JMenuItem("Open");
+        JMenuItem file1 = new JMenuItem("Open File");
+        Image img = new ImageIcon(this.getClass().getResource("Open-file-icon.png")).getImage();
+        file1.setIcon(new ImageIcon(img));
+        
         JMenuItem file2 = new JMenuItem("Save");
+        Image save = new ImageIcon(this.getClass().getResource("Save-icon.png")).getImage();
+        file2.setIcon(new ImageIcon(save));
+        
         JMenuItem file3 = new JMenuItem("Save As");
+        Image saveas = new ImageIcon(this.getClass().getResource("Save-icon.png")).getImage();
+        file3.setIcon(new ImageIcon(saveas));
+        
         JMenuItem edit1 = new JMenuItem("Reset");
-        JMenuItem tools1 = new JMenuItem("Brightness/Contrast");
+        JMenuItem tools1 = new JMenuItem("Brightness");
         JMenuItem tools2 = new JMenuItem("Resize");
         // add menu items to menu
         file.add(file1);
+        
+        JSeparator separator_1 = new JSeparator();
+        file.add(separator_1);
         file.add(file2);
         file.add(file3);
         edit.add(edit1);
         tools.add(tools1);
+        
+        JMenuItem mntmNewMenuItem_1 = new JMenuItem("Contrast");
+        tools.add(mntmNewMenuItem_1);
         tools.add(tools2);
         // add menu to menu bar
         mb.add(file);
+        
+        JSeparator separator = new JSeparator();
+        file.add(separator);
+        
+        JMenuItem mntmNewMenuItem = new JMenuItem("Exit");
+        Image exit = new ImageIcon(this.getClass().getResource("exit-icon.png")).getImage();
+        mntmNewMenuItem.setIcon(new ImageIcon(exit));
+        mntmNewMenuItem.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		System.exit(JFrame.EXIT_ON_CLOSE);
+        	}
+        });
+        file.add(mntmNewMenuItem);
         mb.add(edit);
         mb.add(tools);
+        
+        JMenuItem mntmNewMenuItem_2 = new JMenuItem("Invert");
+        tools.add(mntmNewMenuItem_2);
         // add menubar to panel
         menuBar.setLayout(new BorderLayout());
         menuBar.add(mb);
+        
+        JMenu mnNewMenu = new JMenu("Analyze");
+        mb.add(mnNewMenu);
         l_c.add(menuBar, BorderLayout.NORTH);
         setVisible(true);
         pack();
